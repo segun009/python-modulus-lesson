b88b65a937412432e99e1228a9c3b07684c0390b
segun009 <59865489+segun009@users.noreply.github.com>
2025-05-24 12:54:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# # import process
+# from process import balance as bal
+# from process import add_num
+
+# balance = 40000
+# # print(process.my_name)
+# # print(process.age)
+# # print(process.sentence)
+# print(balance)
+# print(bal)
+# print(add_num(6, 9))
+
+
+
+
+import random as ran
+
+cars = ["teddy", "5naira", "1naira", "iphone"]
+bias = [1, 2, 5, 0]
+
+our_choices = ran.choices(cars, weights=bias, k = 1)
+print(our_choices)
+# sample = ran.sample(cars, k = 2)
+# print(sample)
+# our_choice = ran.choice(cars)
+# print(our_choice)
+# ran.shuffle(cars)
+# print(cars)
+# guess_int = ran.randint(1, 6)
+# guess_float = ran.uniform(1, 6)
+# any_float = ran.random()
+# print(any_float)
---
+++ process.py
@@ -0,0 +1,9 @@
+my_name = "shegun"
+
+age = 70
+
+sentence = "this is an external file from process file"
+balance = 300000
+
+def add_num(x, y):
+    return x + y
